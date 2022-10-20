307f30d249f6297a7b67bea9794591ac36a68bd1
tmax818 <tylermaxwell661@gmail.com>
2022-10-20 14:52:34 -0700

    init solution branch

--- src/App.js
+++ src/App.js
@@ -3,9 +3,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <h1>Hello Dojo!</h1>
         <h3>Things I need to do:</h3>
+        <ul>
+            <li>Learn React</li>
+            <li>Climb Mt. Everest</li>
+            <li>Run a marathon</li>
+            <li>feed the dogs</li>
+        </ul>
 
     </div>
   );
